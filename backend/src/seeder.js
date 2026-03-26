1c3644b0c9bd08f84eb983a37a381274cdb9a59d
arjun virk <asv0773@gmail.com>
2026-03-26 19:03:01 +0530

    data seeded with users and products data

---
+++ backend/src/seeder.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import users from "./data/users.js";
+import products from "./data/products.js";
+import User from "./models/User.js";
+import Product from "./models/Product.js";
+
+dotenv.config();
+
+connectDB();
+
+const importData = async () => {
+  try {
+    await Product.deleteMany();
+    await User.deleteMany();
+
+    const createdUser = await User.insertMany(users);
+    const adminUser = createdUser[0]._id;
+
+    const sampleProducts = products.map((product) => {
+      return { ...product, user: adminUser };
+    });
+
+    await Product.insertMany(sampleProducts);
+
+    console.log("data imported!");
+    process.exit();
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+const destroyData = async () => {
+  try {
+    await Product.deleteMany();
+    await User.deleteMany();
+
+    console.log("data destroyed!");
+    process.exit();
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+if (process.argv[2] === "-d") {
+  destroyData();
+} else {
+  importData();
+}
