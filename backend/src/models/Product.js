a67c3d2e252212b1c2a7c21fe79a8ef3c7b71901
arjun virk <asv0773@gmail.com>
2026-03-26 18:50:41 +0530

    make user and product schema

---
+++ backend/src/models/Product.js
@@ -0,0 +1,72 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const reviewSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    countInStocks: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    reviews: [reviewSchema],
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  { timestamps: true },
+);
+
+let Product = mongoose.model("Product", productSchema);
+export default Product;
